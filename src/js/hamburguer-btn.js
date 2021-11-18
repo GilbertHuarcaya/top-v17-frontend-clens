61f1720e2bdb5dbec6101fff7f70ce336b2c4e03
Carlos <cajavi89@gmail.com>
2021-11-18 17:21:02 -0500

    inicio de maquetacion mobile first

---
+++ src/js/hamburguer-btn.js
@@ -0,0 +1,22 @@
+const d = document;
+
+const menuBtn= d.getElementById("btn-menu-id")
+const menuNav= d.getElementById("menu-id")
+
+
+d.addEventListener("click",e=>{
+
+  if(e.target.matches("#btn-menu-id") || e.target.matches(`#${"btn-menu-id"} *`)){
+    menuBtn.classList.toggle('change-menu')
+    menuNav.classList.toggle('is-active')
+  }
+  if(e.target.matches("#menu-id") || e.target.matches(`#${"menu-id"} *`)){
+      menuBtn.classList.toggle('change-menu')
+      menuNav.classList.toggle('is-active')
+      console.log(e.target);
+    }
+})
+
+
+
+
