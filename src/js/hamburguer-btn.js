a96fe5450b3d60b3a4d305061edc5d9540ad1acf
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-11-18 20:49:08 -0500

    login page for mobile done added logo and fixed js

--- src/js/hamburguer-btn.js
+++ src/js/hamburguer-btn.js
@@ -1,22 +1,19 @@
 const d = document;
 
-const menuBtn= d.getElementById("btn-menu-id")
-const menuNav= d.getElementById("menu-id")
+const menuBtn = d.getElementById('btn-menu-id');
+const menuNav = d.getElementById('menu-id');
 
-
-d.addEventListener("click",e=>{
-
-  if(e.target.matches("#btn-menu-id") || e.target.matches(`#${"btn-menu-id"} *`)){
-    menuBtn.classList.toggle('change-menu')
-    menuNav.classList.toggle('is-active')
+d.addEventListener('click', (e) => {
+  if (
+    e.target.matches('#btn-menu-id')
+    || e.target.matches(`#${'btn-menu-id'} *`)
+  ) {
+    menuBtn.classList.toggle('change-menu');
+    menuNav.classList.toggle('is-active');
   }
-  if(e.target.matches("#menu-id") || e.target.matches(`#${"menu-id"} *`)){
-      menuBtn.classList.toggle('change-menu')
-      menuNav.classList.toggle('is-active')
-      console.log(e.target);
-    }
-})
-
-
-
-
+  if (e.target.matches('#menu-id') || e.target.matches(`#${'menu-id'} *`)) {
+    menuBtn.classList.toggle('change-menu');
+    menuNav.classList.toggle('is-active');
+    console.log(e.target);
+  }
+});
