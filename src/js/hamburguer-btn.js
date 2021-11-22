97e33552e94050c2c1751d4e068fedbe94a6db31
Carlos <cajavi89@gmail.com>
2021-11-22 07:20:12 -0500

    agregado menú usuario

--- src/js/hamburguer-btn.js
+++ src/js/hamburguer-btn.js
@@ -2,6 +2,9 @@ const d = document;
 
 const menuBtn = d.getElementById('btn-menu-id');
 const menuNav = d.getElementById('menu-id');
+const menuPerfil = d.getElementById('menu-perfil');
+const perfil = d.getElementById('perfil');
+
 
 d.addEventListener('click', (e) => {
   if (
@@ -14,6 +17,12 @@ d.addEventListener('click', (e) => {
   if (e.target.matches('#menu-id') || e.target.matches(`#${'menu-id'} *`)) {
     menuBtn.classList.toggle('change-menu');
     menuNav.classList.toggle('is-active');
-    console.log(e.target);
+  }
+  if(e.target === perfil ){
+    menuPerfil.classList.toggle('is-active-menu-perfil')
+  }
+
+  if(e.target === menuPerfil || e.target.matches(`#${'menu-perfil'} *`)){
+    menuPerfil.classList.toggle('is-active-menu-perfil')
   }
 });
