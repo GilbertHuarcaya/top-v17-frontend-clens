63d37217edffac053adb7e289bf4cd314906aace
Carlos <cajavi89@gmail.com>
2021-11-25 08:07:41 -0500

    agregado componente 'HowItWorks' y 2 sub-componentes 'TitleMinititle y SectionInfo'

---
+++ src/components/SectionInfo/index.jsx
@@ -0,0 +1,11 @@
+import './styles.scss';
+// eslint-disable-next-line react/prop-types
+const SectionInfo = ({ title = '', info = '', imgn }) => (
+  <section className="section-info">
+    {imgn ? <img className="section-info__img" src={imgn} alt={title} /> : null }
+    <h3 className="section-info__title">{title}</h3>
+    <p className="section-info__info">{info}</p>
+  </section>
+);
+
+export default SectionInfo;
