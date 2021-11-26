24e73d87d8780eeca9d09ecc9bc5664eab9ea242
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-11-26 12:27:08 -0500

    created account and home pages fixed account styles to fit the page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import HeroSection from '../../components/HeroSection';
+import HowItWorks from '../../components/HowItWorks/index';
+
+const App = () => (
+  <>
+    <Header />
+    <HeroSection />
+    <HowItWorks />
+    <Footer />
+  </>
+);
+export default App;
