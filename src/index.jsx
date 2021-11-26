30328a86eed1d31b472b8107c4796b1fb896c577
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-11-26 12:21:55 -0500

    apllied media queries to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Account from './pages/Account/Account';
 
 import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
+    <Home />
+    <Account />
+    <Register />
   </React.StrictMode>,
   document.getElementById('root'),
 );
