34a16c902299bbba4783392c247d819706241f82
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-11-26 12:23:51 -0500

    created login and register pages and updated atyles to fit the pages

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import LoginForm from '../../components/LoginForm/LoginForm';
+
+const Login = () => (
+  <>
+    <Header />
+    <LoginForm />
+    <Footer />
+  </>
+);
+export default Login;
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import RegisterForm from '../../components/RegisterForm/RegisterForm';
+
+const Register = () => (
+  <>
+    <Header />
+    <RegisterForm />
+    <Footer />
+  </>
+);
+export default Register;
