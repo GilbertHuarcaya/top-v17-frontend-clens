a3b323abe110e508f208f2ff78380e2cb2da2576
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-11-26 12:22:38 -0500

    fixed hero section image

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,9 +1,7 @@
-import heroImage from '../../img/hero-image.jpg';
 import './styles.scss';
 
 const HeroSection = () => (
   <section className="hero-section">
-    <img className="hero-section__image" src={heroImage} alt="imagen-hero" />
     <h2 className="hero-section__title">El mejor servicio, al mejor precio</h2>
     <p className="hero-section__information">Personal experimentado. Todo gestionado online. Un servicio de limpieza que sí funciona.</p>
     <button className="hero-section--button" type="button">Mira los planes</button>
