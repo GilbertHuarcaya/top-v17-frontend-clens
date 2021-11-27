a068cfdd35df83721a4463535aff22e5ff77172a
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-11-27 18:23:48 -0500

    Created and aplied services section to home

---
+++ src/components/ServicesHome/index.jsx
@@ -0,0 +1,15 @@
+import TitleMinititle from '../TitleMinititle';
+import Cards from '../Cards/Index';
+import './styles.scss';
+
+const ServicesHome = () => (
+  <>
+    <TitleMinititle
+      title="Servicios Clens"
+      minititle="Tenemos altos estandares para un servicio de limpieza minucioso y atento. Al recibir un servicio de limpieza de Clens puedes estar seguro que es calidad. Esto está incluido en cada servicio:"
+    />
+    <Cards />
+  </>
+);
+
+export default ServicesHome;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import HeroSection from '../../components/HeroSection';
 import HowItWorks from '../../components/HowItWorks/index';
+import ServicesHome from '../../components/ServicesHome/index';
 
 const App = () => (
   <>
     <HeroSection />
     <HowItWorks />
+    <ServicesHome />
   </>
 );
 export default App;
