10f0c55012bdce598ebeb3011620ca88f994a32c
AlonsoGaray <alonsogarayp@gmail.com>
2021-11-27 19:57:42 -0500

    Funcionalidades agregadas para los 'botones' de los ojos. Y tambien ruta cambiada para info-acount

--- src/components/InfoCuenta/InfoCuenta.jsx
+++ src/components/InfoCuenta/InfoCuenta.jsx
@@ -1,84 +1,131 @@
 import './InfoCuenta.scss';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faImage } from '@fortawesome/free-solid-svg-icons';
 import info from '../../img/info-cuenta.png';
 
-const InfoCuenta = () => (
-  <div className="card--desktop">
-    <div className="card">
-      <form className="card__form">
-        <h2 className="card__form__h2">Información de Cuenta</h2>
-        <h5 className="card__form__h5">
-          Aqui puede actualizar su información:
-        </h5>
-        <div className="card__form__group">
-          <p className="card__form__titulo">Dirección</p>
-          <input
-            type="text"
-            id="input-direccion"
-            className="card__form__input"
-            defaultValue="Direccion Actual"
-          />
-        </div>
-        <div className="card__form__group">
-          <p className="card__form__titulo">Nombre Completo</p>
-          <input
-            type="text"
-            id="input-nombre"
-            className="card__form__input"
-            defaultValue="Nombre Actual"
-          />
-        </div>
-        <div className="card__form__group">
-          <p className="card__form__titulo">Teléfono</p>
-          <input
-            type="tel"
-            id="input-telefono"
-            className="card__form__input"
-            defaultValue="Telefono Actual"
-          />
-        </div>
+const image = <FontAwesomeIcon icon={faImage} />;
+const eye = <FontAwesomeIcon icon={faEye} />;
 
-        <h4 className="card__form__h4">Cambie su información de ingreso</h4>
-        <div className="card__form__group">
-          <p className="card__form__titulo">Correo</p>
-          <input
-            type="email"
-            className="card__form__input"
-            defaultValue="Correo_actual@gmail.com"
-          />
-        </div>
-        <div className="card__form__group">
-          <p className="card__form__titulo">Contraseña antigua</p>
-          <div className="pass-eye">
+const InfoCuenta = () => {
+  const [actualPasswordShown, setActualPasswordShown] = useState(false);
+  const toggleActualPasswordVisiblity = () => {
+    setActualPasswordShown(!actualPasswordShown);
+  };
+
+  const onKeyDownActual = (e) => {
+    if (e.keyCode === 13 || e.keyCode === 32) {
+      setActualPasswordShown(!actualPasswordShown);
+    }
+  };
+
+  const [newPasswordShown, setNewPasswordShown] = useState(false);
+  const toggleNewPasswordVisiblity = () => {
+    setNewPasswordShown(!newPasswordShown);
+  };
+
+  const onKeyDownNew = (e) => {
+    if (e.keyCode === 13 || e.keyCode === 32) {
+      setNewPasswordShown(!newPasswordShown);
+    }
+  };
+
+  return (
+    <div className="card--desktop">
+      <div className="card">
+        <form className="card__form">
+          <h2 className="card__form__h2">Información de Cuenta</h2>
+          <h5 className="card__form__h5">
+            Aqui puede actualizar su información:
+          </h5>
+          <div className="card__form__group">
+            <p className="card__form__titulo">Dirección</p>
             <input
-              type="password"
+              type="text"
+              id="input-direccion"
               className="card__form__input"
-              id="old-password"
+              defaultValue="Direccion Actual"
             />
-            <span className="far fa-eye" id="toggleOldPassword" />
           </div>
-        </div>
-        <div className="card__form__group">
-          <p className="card__form__titulo">Nueva contraseña</p>
-          <div className="pass-eye">
+          <div className="card__form__group">
+            <p className="card__form__titulo">Nombre Completo</p>
             <input
-              type="password"
+              type="text"
+              id="input-nombre"
               className="card__form__input"
-              id="new-password"
+              defaultValue="Nombre Actual"
             />
-            <span className="far fa-eye" id="toggleNewPassword" />
           </div>
-        </div>
-        <div className="card__form__group">
-          <p className="card__form__titulo">Foto</p>
-          <input type="text" className="card__form__input" />
-        </div>
-        <button type="submit" className="card__form__btn">
-          Actualizar
-        </button>
-      </form>
-      <img className="info__img" src={info} alt="" />
+          <div className="card__form__group">
+            <p className="card__form__titulo">Teléfono</p>
+            <input
+              type="tel"
+              id="input-telefono"
+              className="card__form__input"
+              defaultValue="Telefono Actual"
+            />
+          </div>
+
+          <h4 className="card__form__h4">Cambie su información de ingreso</h4>
+          <div className="card__form__group">
+            <p className="card__form__titulo">Correo</p>
+            <input
+              type="email"
+              className="card__form__input"
+              defaultValue="Correo_actual@gmail.com"
+            />
+          </div>
+          <div className="card__form__group">
+            <p className="card__form__titulo">Contraseña actual</p>
+            <div className="pass-eye">
+              <input
+                type={actualPasswordShown ? 'text' : 'password'}
+                className="card__form__input"
+                id="old-password"
+              />
+              <i
+                role="button"
+                tabIndex="0"
+                onKeyDown={onKeyDownActual}
+                onClick={toggleActualPasswordVisiblity}
+              >
+                {eye}
+              </i>
+            </div>
+          </div>
+          <div className="card__form__group">
+            <p className="card__form__titulo">Nueva contraseña</p>
+            <div className="pass-eye">
+              <input
+                type={newPasswordShown ? 'text' : 'password'}
+                className="card__form__input"
+                id="new-password"
+              />
+              <i
+                role="button"
+                tabIndex="0"
+                onKeyDown={onKeyDownNew}
+                onClick={toggleNewPasswordVisiblity}
+              >
+                {eye}
+              </i>
+            </div>
+          </div>
+          <div className="card__form__group">
+            <p className="card__form__titulo">Foto</p>
+            <input type="text" className="card__form__input" />
+            <i role="button" tabIndex="0" onKeyDown={onKeyDownNew}>
+              {image}
+            </i>
+          </div>
+          <button type="submit" className="card__form__btn">
+            Actualizar
+          </button>
+        </form>
+        <img className="info__img" src={info} alt="" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default InfoCuenta;
