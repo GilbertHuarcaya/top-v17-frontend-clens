4b10b87df0fc92d9c86797d42feaa7b251f52bd0
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-02 22:36:38 -0500

    Fixed reviews section in Home

--- src/components/CardsReseñas/index.jsx
+++ src/components/CardsReseñas/index.jsx
@@ -1,27 +1,75 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
 
 const CardsReseñas = (props) => {
   const { reseñas } = props;
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const check = (index) => setSelectedIndex(index);
+
+  const checkNext = () => {
+    const labels = document.querySelectorAll('#slider label');
+    const nextIndex =
+      selectedIndex === labels.length - 1 ? 0 : selectedIndex + 1;
+    setSelectedIndex(nextIndex);
+  };
+  const checkBefore = () => {
+    const labels = document.querySelectorAll('#slider label');
+    const nextIndex =
+      selectedIndex === 0 ? labels.length - 1 : selectedIndex - 1;
+    setSelectedIndex(nextIndex);
+  };
 
   return (
-    <div className="review-cards">
-      {reseñas.map((e) => (
-        <div className="review-card" id={e.id} key={`id-${e.id}`}>
-          <div className="review-card__comment">
-            <h3>{e.title}</h3>
-            <p>{e.info}</p>
-          </div>
-          <div className="review-card__client">
-            <div className="review-card__picture">
-              <img src={e.img} alt={e.id} />
+    <div className="review-cards" id="review-cards">
+      <button
+        className="review-cards__button-before"
+        type="button"
+        onClick={checkBefore}
+      >
+        {'<'}
+      </button>
+      <section id="slider">
+        {reseñas.map((e) => (
+          <input
+            type="radio"
+            name="slider"
+            id={`s${e.reviewid}`}
+            checked={selectedIndex === e.reviewid - 1}
+            onClick={() => check(e.reviewid - 1)}
+            key={`id--${e.reviewid}`}
+          />
+        ))}
+        {reseñas.map((e) => (
+          <label
+            htmlFor={`s${e.reviewid}`}
+            id={`slide${e.reviewid}`}
+            className="review-card"
+            key={`id-${e.reviewid}`}
+          >
+            <div className="review-card__comment">
+              <h3>{e.title}</h3>
+              <p>{e.info}</p>
             </div>
-            <div className="review-card__name">
-              <h4>{e.client}</h4>
+            <div className="review-card__client">
+              <div className="review-card__picture">
+                <img src={e.img} alt={e.reviewid} />
+              </div>
+              <div className="review-card__name">
+                <h4>{e.client}</h4>
+              </div>
             </div>
-          </div>
-        </div>
-      ))}
+          </label>
+        ))}
+      </section>
+      <button
+        className="review-cards__button-next"
+        type="button"
+        onClick={checkNext}
+      >
+        {'>'}
+      </button>
     </div>
   );
 };
--- src/components/ReviewsHome/index.jsx
+++ src/components/ReviewsHome/index.jsx
@@ -1,7 +1,6 @@
 import './styles.scss';
 import MinititleTitle from '../MinititleTitle';
 import CardsReseñas from '../CardsReseñas';
-import ReviewNav from '../ReviewNav';
 
 const ReviewsHome = () => {
   const reseñas = [
@@ -10,23 +9,24 @@ const ReviewsHome = () => {
       info: 'El servicio fue muy puntual y coordial, el personal se identifico adecuadamente y terminaron a la hora pactada, próximamente volveré a contactarlos',
       client: 'Pedro Becerra',
       img: 'https://randomuser.me/api/portraits/men/56.jpg',
-      id: 'review1',
+      reviewid: '1',
     },
     {
       title: '¡Son muy puntuales!',
       info: 'Llevo 2 meses contratando el servicio mensual de Clens y estoy encantado, agendo los días con anticipación el primer día del mes y siempre llegan puntuales',
       client: 'María Manuela',
       img: 'https://randomuser.me/api/portraits/women/54.jpg',
-      id: 'review2',
+      reviewid: '2',
     },
     {
       title: 'Muy cordiales',
       info: 'Me gusta Clens por que su limpieza es bastante detallada y los chicos no tienen problemas en ayudarme con cualquier cosa extra que les pida acorde al servicio contratado',
       client: 'Manuel Maldonado',
       img: 'https://randomuser.me/api/portraits/men/34.jpg',
-      id: 'review3',
+      reviewid: '3',
     },
   ];
+
   return (
     <section className="reviews">
       <MinititleTitle
@@ -34,7 +34,6 @@ const ReviewsHome = () => {
         minititle="Reseñas"
       />
       <CardsReseñas reseñas={reseñas} />
-      <ReviewNav />
     </section>
   );
 };
