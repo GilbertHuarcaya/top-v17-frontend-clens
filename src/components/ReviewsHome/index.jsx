369636b5892ca9a93540eac3e5c85dbd68654dbb
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-05 22:23:06 -0500

    Created database data and aplied async functions

--- src/components/ReviewsHome/index.jsx
+++ src/components/ReviewsHome/index.jsx
@@ -1,57 +1,38 @@
+import { useEffect, useState } from 'react';
 import './styles.scss';
 import MinititleTitle from '../MinititleTitle';
 import CardsReviews from '../CardsReviews';
+import { getAllReviews } from './reviews';
 
 const ReviewsHome = () => {
-  const reseñas = [
-    {
-      title: 'Muy cordiales',
-      info: 'El servicio fue muy puntual y coordial, el personal se identifico adecuadamente y terminaron a la hora pactada, próximamente volveré a contactarlos',
-      client: 'Pedro Becerra',
-      img: 'https://randomuser.me/api/portraits/men/56.jpg',
-      reviewid: 1,
-      rate: '⭐⭐⭐⭐⭐',
-    },
-    {
-      title: '¡Son muy puntuales!',
-      info: 'Llevo 2 meses contratando el servicio mensual de Clens y estoy encantado, agendo los días con anticipación el primer día del mes y siempre llegan puntuales',
-      client: 'María Manuela',
-      img: 'https://randomuser.me/api/portraits/women/54.jpg',
-      reviewid: 2,
-      rate: '⭐⭐⭐⭐⭐',
-    },
-    {
-      title: 'Muy cordiales',
-      info: 'Me gusta Clens por que su limpieza es bastante detallada y los chicos no tienen problemas en ayudarme con cualquier cosa extra que les pida acorde al servicio contratado',
-      client: 'Manuel Maldonado',
-      img: 'https://randomuser.me/api/portraits/men/34.jpg',
-      reviewid: 3,
-      rate: '⭐⭐⭐⭐',
-    },
-    {
-      title: 'Muy cordiales',
-      info: 'Me gusta Clens por que su limpieza es bastante detallada y los chicos no tienen problemas en ayudarme con cualquier cosa extra que les pida acorde al servicio contratado',
-      client: 'Juan Ferrer',
-      img: 'https://randomuser.me/api/portraits/men/37.jpg',
-      reviewid: 4,
-      rate: '⭐⭐',
-    },
-    {
-      title: 'Muy cordiales',
-      info: 'Me gusta Clens por que su limpieza es bastante detallada y los chicos no tienen problemas en ayudarme con cualquier cosa extra que les pida acorde al servicio contratado',
-      client: 'Jorge Mamani',
-      img: 'https://randomuser.me/api/portraits/men/39.jpg',
-      reviewid: 5,
-      rate: '⭐⭐⭐',
-    },
-  ];
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const getReviews = async () => {
+      try {
+        const data = await getAllReviews();
+        setTimeout(() => {
+          setReviews(data);
+        }, 1000);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      }
+    };
+    getReviews();
+  }, []);
+
   return (
     <section className="reviews">
       <MinititleTitle
         title="Reseñas de nuestros clientes"
         minititle="Reseñas"
       />
-      <CardsReviews reseñas={reseñas} />
+      {reviews.length > 0 ? (
+        <CardsReviews reseñas={reviews} />
+      ) : (
+        <h1>Loading ...</h1>
+      )}
     </section>
   );
 };
--- src/components/ServicesHome/index.jsx
+++ src/components/ServicesHome/index.jsx
@@ -1,66 +1,27 @@
+import { useEffect, useState } from 'react';
 import MinititleTitle from '../MinititleTitle';
 import CardsServices from '../CardsServices';
 import './styles.scss';
+import getAllServices from './services';
 
 const ServicesHome = () => {
-  const services = [
-    {
-      title: 'Cocinas',
-      info: 'Suministramos productos Biodegradables para la implementación de Programas de Limpieza y Desinfección.',
-      img: 'https://images.unsplash.com/photo-1567767326925-e2047bf469d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-      serviceid: 1,
-      items: [
-        'Pisos',
-        'Brillado en cromos',
-        'Lunas',
-        'Campana',
-        'Desempolvar superficies',
-        'Hornillas y lavaderos',
-      ],
-    },
-    {
-      title: 'Baños',
-      info: 'Suministramos productos Biodegradables para la implementación de Programas de Limpieza y Desinfección y  productos para el cuidado de pisos, Higiene de Manos, limpieza y Desinfección de Baños.',
-      img: 'https://images.unsplash.com/flagged/photo-1556438758-84625859c6b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3274&q=80',
-      serviceid: 2,
-      items: [
-        'Pisos',
-        'Brillado en cromos',
-        'Espejos',
-        'Lunas de duchas',
-        'Mayolicas',
-        'Desempolvar gabinetes',
-      ],
-    },
-    {
-      title: 'Salas',
-      info: 'Suministramos productos Biodegradables para la implementación de Programas de Limpieza y Desinfección y productos para el cuidado de pisos, Higiene de Manos, limpieza y Desinfección de zonas amplias como salas y salones.',
-      img: 'https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-      serviceid: 3,
-      items: [
-        'Pisos',
-        'Brillado en cromos',
-        'Lunas',
-        'Limpieza muebles y tapiz',
-        'Desempolvar superficies',
-        'Desempolvar cortinas',
-      ],
-    },
-    {
-      title: 'Habitaciones',
-      info: 'Suministramos productos Biodegradables para la implementación de Programas de Limpieza y Desinfección y productos para el cuidado de pisos, Higiene de Manos, limpieza y Desinfección de habitaciones y muebles.',
-      img: 'https://images.unsplash.com/photo-1445991842772-097fea258e7b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-      serviceid: 4,
-      items: [
-        'Pisos',
-        'Cama',
-        'Alfombras',
-        'Cajones',
-        'Limpieza muebles y tapiz',
-        'Desempolvar cortinas',
-      ],
-    },
-  ];
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    const getServices = async () => {
+      try {
+        const data = await getAllServices();
+        setTimeout(() => {
+          setServices(data);
+        }, 1000);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      }
+    };
+    getServices();
+  }, []);
+
   return (
     <section className="home-services">
       <MinititleTitle
@@ -68,7 +29,11 @@ const ServicesHome = () => {
         title="Vamos más allá que solo limpieza"
         info="Tenemos altos estandares para un servicio de limpieza minucioso y atento. Al recibir un servicio de limpieza de Clens puedes estar seguro que es calidad. Esto está incluido en cada servicio:"
       />
-      <CardsServices services={services} />
+      {services.length > 0 ? (
+        <CardsServices services={services} />
+      ) : (
+        <h1>Loading ...</h1>
+      )}
     </section>
   );
 };
