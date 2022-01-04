ff44ecaab926f46d75fef1cdc6c0e49f4394d634
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-01-04 02:00:06 -0500

    Added scrool up on render

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,22 @@
+import { useEffect } from 'react';
 import HeroSection from '../../components/HeroSection';
 import HowItWorks from '../../components/HowItWorks';
 import ServicesHome from '../../components/ServicesHome';
 import BtnSolicita from '../../components/BtnSolicita';
 import ReviewsHome from '../../components/ReviewsHome';
 
-const App = () => (
-  <>
-    <HeroSection />
-    <HowItWorks />
-    <ServicesHome />
-    <ReviewsHome />
-    <BtnSolicita />
-  </>
-);
+const App = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <>
+      <HeroSection />
+      <HowItWorks />
+      <ServicesHome />
+      <ReviewsHome />
+      <BtnSolicita />
+    </>
+  );
+};
 export default App;
