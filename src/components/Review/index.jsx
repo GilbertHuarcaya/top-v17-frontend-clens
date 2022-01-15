328c1d94c249024b82a0ad92d69e98ace341af32
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-01-15 15:42:59 -0500

    cambiado el valor de star rating de 0 a 100

--- src/components/Review/index.jsx
+++ src/components/Review/index.jsx
@@ -1,31 +1,64 @@
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { postUserReview } from '../../store/actions';
+import { Rating } from 'react-simple-star-rating';
+import {
+  postUserReview,
+  patchUserOrder,
+  getUserOrdersFromDB,
+} from '../../store/actions';
 import useForm from '../../hooks/useForm';
+import './styles.scss';
 
-const Review = () => {
-  const user = useSelector((state) => state.user);
-  const navigate = useNavigate();
+// eslint-disable-next-line react/prop-types
+const Review = ({ reviewOrder }) => {
+  const [reviewRating, setReviewRating] = useState(0);
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+
   let prefilledForm = {};
   if (user) {
     prefilledForm = {
       userId: user.id,
       userName: user.userName,
-      userPhoto: 'http.google.com',
-      comentario: '',
-      rating: '3',
+      userPhoto: `https://randomuser.me/api/portraits/men/${Math.floor(
+        Math.random() * (100 - 1 + 1) + 1,
+      )}.jpg`,
     };
   } else {
     prefilledForm = {};
   }
   const { form, handleChange } = useForm(prefilledForm);
+  const [formOk, setFormOk] = useState(false);
+
+  useEffect(() => {
+    const validateForm = () => {
+      try {
+        if (form.comentario && reviewRating > 0) {
+          setFormOk(true);
+        }
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      }
+    };
+    validateForm();
+  }, [handleChange]);
+
+  const proceedToReviewOrder = async (order) => {
+    // eslint-disable-next-line no-console
+    await patchUserOrder(dispatch, { ...order, reviewed: true });
+    await getUserOrdersFromDB(dispatch, user.id);
+  };
+
+  const handleRating = (rate) => {
+    setReviewRating(rate);
+    // other logic
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    /* loginUser(dispatch, form); */
-    postUserReview(dispatch, form);
-    navigate('/');
+    await postUserReview(dispatch, { ...form, rating: reviewRating });
+    proceedToReviewOrder(reviewOrder);
   };
   return (
     <div className="review">
@@ -44,7 +77,23 @@ const Review = () => {
             </div>
           </label>
         </div>
-        <button className="btn btn-primary" id="btn-continue" type="submit">
+        <div className="form-group">
+          <div className="control-label" htmlFor="email">
+            Rating
+            <div className="slot">
+              <Rating
+                onClick={handleRating}
+                ratingValue={reviewRating} /* Available Props */
+              />
+            </div>
+          </div>
+        </div>
+        <button
+          className="btn btn-primary"
+          id="btn-continue"
+          disabled={!formOk}
+          type="submit"
+        >
           Enviar
         </button>
       </form>
