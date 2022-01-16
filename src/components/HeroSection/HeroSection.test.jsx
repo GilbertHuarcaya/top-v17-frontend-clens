55376f8fa67085977c6adca2f3a3c6608b498317
Carlos <cajavi89@gmail.com>
2022-01-16 13:31:35 -0500

    test for HeroSection finished

--- src/components/HeroSection/HeroSection.test.jsx
+++ src/components/HeroSection/HeroSection.test.jsx
@@ -3,8 +3,19 @@ import { render, screen } from '@testing-library/react';
 
 import HeroSection from './index';
 
-test('render component', () => {
+test('render HeroSection and title,information and button', () => {
   render(<HeroSection />);
-  const buttonSeePlans = screen.getByText(/Mira los planes/i);
+  // encontrar el titulo
+  const title = screen.getByText(/el mejor servicio, al mejor precio/i);
+  expect(title).toBeInTheDocument();
+  // encontrar el texto informacion
+  const information = screen.getByText(
+    /Personal experimentado. Todo gestionado online. Un servicio de limpieza que sí funciona./i,
+  );
+  expect(information).toBeInTheDocument();
+  // encontrar el boton ver los planes
+  const buttonSeePlans = screen.getByRole('button', {
+    name: /Mira los planes/i,
+  });
   expect(buttonSeePlans).toBeInTheDocument();
 });
