86f6961347684d11db558bca33b524fcd1018601
Carlos <cajavi89@gmail.com>
2022-01-26 21:25:01 -0500

    component, styles first version

--- src/components/MisServicios/index.jsx
+++ src/components/MisServicios/index.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './styles.scss';
 
 const MisServicios = () => {
+  const user = useSelector((state) => state.user);
+
   return (
     <section className="my-services-wrapper">
-      <article className="my-services-wrapper__services-assigned">
-        <h2>Servicios Asignados</h2>
-      </article>
-      <article className="my-services-wrapper__services-finished">
-        <h2>Servicios Terminados</h2>
-      </article>
-      <article className="my-services-wrapper__benefits-earned">
-        <h2>Dinero Ganado</h2>
-      </article>
+      <h2 className="my-services-wrapper__user-name">
+        Hola <span>{user?.fullname.toUpperCase()}</span>, estos son tus
+        servicios:
+      </h2>
+      <section className="my-services-wrapper__grid-container">
+        <article className="my-services-wrapper__grid-container-services-assigned">
+          <h2>Servicios Asignados</h2>
+        </article>
+        <article className="my-services-wrapper__grid-container-services-finished">
+          <h2>Servicios Terminados</h2>
+        </article>
+        <article className="my-services-wrapper__grid-container-benefits-earned">
+          <h2>Dinero Ganado</h2>
+        </article>
+      </section>
     </section>
   );
 };
