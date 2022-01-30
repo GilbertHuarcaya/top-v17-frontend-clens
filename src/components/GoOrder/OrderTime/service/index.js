376f1dba40607e47be01fa2c0b127055fcb53244
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-01-29 21:12:13 -0500

    horadelegada acomodada para encajar con hora individual de disponibilidad de personal clens

--- src/components/GoOrder/OrderTime/service/index.js
+++ src/components/GoOrder/OrderTime/service/index.js
@@ -2,11 +2,13 @@ export const createMorningToday = () => {
   const Array = [];
   for (let i = 7; i < 12; i += 1) {
     const hora = {
-      hora: `${i}:00 - ${i}:30`,
+      hora: `${i < 10 ? 0 : ''}${i}:00`,
+      horaLlegada: `${i < 10 ? 0 : ''}${i}:00 - ${i}:30`,
       disabled: i < new Date().getHours() + 1,
     };
     const nextHora = {
-      hora: `${i}:30 - ${i + 1}:00`,
+      hora: `${i < 10 ? 0 : ''}${i}:30`,
+      horaLlegada: `${i < 10 ? 0 : ''}${i}:30 - ${i + 1}:00`,
       disabled: i < new Date().getHours() + 1,
     };
     Array.push(hora);
@@ -18,11 +20,13 @@ export const createAfternoonToday = () => {
   const Array = [];
   for (let i = 13; i < 18; i += 1) {
     const hora = {
-      hora: `${i}:00 - ${i}:30`,
+      hora: `${i}:00`,
+      horaLlegada: `${i}:00 - ${i}:30`,
       disabled: i < new Date().getHours() + 1,
     };
     const nextHora = {
-      hora: `${i}:30 - ${i + 1}:00`,
+      hora: `${i}:30`,
+      horaLlegada: `${i}:30 - ${i + 1}:00`,
       disabled: i < new Date().getHours() + 1,
     };
     Array.push(hora);
@@ -33,8 +37,16 @@ export const createAfternoonToday = () => {
 export const createMorning = () => {
   const Array = [];
   for (let i = 7; i < 12; i += 1) {
-    const hora = { hora: `${i}:00 - ${i}:30`, disabled: false };
-    const nextHora = { hora: `${i}:30 - ${i + 1}:00`, disabled: false };
+    const hora = {
+      hora: `${i < 10 ? 0 : ''}${i}:00`,
+      horaLlegada: `${i < 10 ? 0 : ''}${i}:00 - ${i}:30`,
+      disabled: false,
+    };
+    const nextHora = {
+      hora: `${i < 10 ? 0 : ''}${i}:30`,
+      horaLlegada: `${i < 10 ? 0 : ''}${i}:30 - ${i + 1}:00`,
+      disabled: false,
+    };
     Array.push(hora);
     Array.push(nextHora);
   }
@@ -43,8 +55,16 @@ export const createMorning = () => {
 export const createAfternoon = () => {
   const Array = [];
   for (let i = 13; i < 18; i += 1) {
-    const hora = { hora: `${i}:00 - ${i}:30`, disabled: false };
-    const nextHora = { hora: `${i}:30 - ${i + 1}:00`, disabled: false };
+    const hora = {
+      hora: `${i}:00`,
+      horaLlegada: `${i}:00 - ${i}:30`,
+      disabled: false,
+    };
+    const nextHora = {
+      hora: `${i}:30`,
+      horaLlegada: `${i}:30 - ${i + 1}:00`,
+      disabled: false,
+    };
     Array.push(hora);
     Array.push(nextHora);
   }
