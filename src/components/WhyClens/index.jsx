fa4deaa6d7fcb25b1f9d2abf7fc9feb1a1bd8749
Carlos <cajavi89@gmail.com>
2022-01-31 17:01:34 -0500

    images changees

--- src/components/WhyClens/index.jsx
+++ src/components/WhyClens/index.jsx
@@ -11,22 +11,23 @@ const WhyClens = () => {
         <CardWhyClens
           title="Personal profesional"
           description="Todos nuestros colaboradores son amigables y confiables. Han sido examinados, verificados de antecedentes y calificados con 5 estrellas"
-          img="https://d17x34b9fcvxk7.cloudfront.net/static/marketing/design-tokens/images/housekeepers/window-washer.c0184b75b554.svg"
+          img="https://res.cloudinary.com/cajavi89/image/upload/v1643664092/clens4_qpvlmc.svg"
         />
         <CardWhyClens
           title="Sin contrato mínimo"
           description="Cuando reserva una limpieza semanal o quincenal periódica, puede cambiar su limpiador o cancelar sus limpiezas en cualquier momento"
-          img="https://d17x34b9fcvxk7.cloudfront.net/static/marketing/design-tokens/images/housekeepers/window-washer.c0184b75b554.svg"
+          img="https://res.cloudinary.com/cajavi89/image/upload/v1643664092/clens1_kpxk0p.svg"
         />
         <CardWhyClens
           title="Control online"
           description="Puede agregar visitas, omitir visitas, dejar notas y reservar servicios adicionales que incluyen planchado, lavandería, nevera y limpieza del horno."
-          img="https://d17x34b9fcvxk7.cloudfront.net/static/marketing/design-tokens/images/housekeepers/window-washer.c0184b75b554.svg"
+          img="https://res.cloudinary.com/cajavi89/image/upload/v1643664092/clens2_znjzls.svg"
         />
         <CardWhyClens
           title="Ofrecemos el mejor servicio"
           description="Puede agregar notas e instrucciones, darles instrucciones precisas de limpieza e incluso chatear con ellos directamente usando nuestra aplicación"
-          img="https://d17x34b9fcvxk7.cloudfront.net/static/marketing/design-tokens/images/housekeepers/window-washer.c0184b75b554.svg"
+          img="https://res.cloudinary.com/cajavi89/image/upload/v1643664092/clens2_znjzls.svg"
+          style={{ height: '150px' }}
         />
       </section>
     </>
