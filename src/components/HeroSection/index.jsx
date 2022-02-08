1c93db1666a75fa0c2fb1c6b027dab48ddb34646
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-02-07 22:16:08 -0500

    metadata updated

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -2,7 +2,9 @@ import './styles.scss';
 
 const HeroSection = () => (
   <section className="hero-section">
-    <h2 className="hero-section__title">El mejor servicio, al mejor precio</h2>
+    <h2 className="hero-section__title">
+      El mejor servicio de limpieza del hogar, al mejor precio
+    </h2>
     <p className="hero-section__information">
       Personal experimentado. Todo gestionado online. Un servicio de limpieza
       que sí funciona.
